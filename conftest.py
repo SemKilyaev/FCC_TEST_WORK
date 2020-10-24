989168f9567dd4ae4589d0a4b36a96f6df3f1a71
SemKilyaev <darkvanted@gmail.com>
2020-10-24 15:53:16 +0300

    Other browser

--- conftest.py
+++ conftest.py
@@ -1,11 +1,25 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
